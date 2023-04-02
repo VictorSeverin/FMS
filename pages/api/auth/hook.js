2496ae659ed283f20d2f68fef0376063fc541489
victor severin <vikea.severin@gmail.com>
2023-04-01 21:43:13 -0700

    hook test

--- pages/api/auth/hook.js
+++ pages/api/auth/hook.js
@@ -3,10 +3,10 @@ import prisma from '../../../lib/prisma';
 
 const handler = async (req, res) => {
     const { email, secret } = req.body;
-    // 1
-    // if (req.method !== 'POST') {
-    //     return res.status(403).json({ message: 'Method not allowed' });
-    // }
+
+    if (req.method !== 'POST') {
+        return res.status(403).json({ message: 'Method not allowed' });
+    }
     // // 2
     // if (secret !== process.env.AUTH0_HOOK_SECRET) {
     //     return res.status(403).json({ message: `You must provide the secret 🤫` });
@@ -17,11 +17,13 @@ const handler = async (req, res) => {
         const res = await prisma.user.create({
             data: {
                 email: email,
+                firstName: "prod",
+                lastName: "test",
+                companyId: 1,
             },
         });
         return res.status(200).json({
-            message: `User with email: ${email} has been created successfully!`,
-            user: res,
+            message: "hello",
         });
     }
 };
--- pages/reports.jsx
+++ pages/reports.jsx
@@ -2,14 +2,23 @@ import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/Navbar.jsx";
 import Sidebar from "../components/Sidebar.jsx";
+import axios from "axios";
+import { handleAuth } from "@auth0/nextjs-auth0";
 //import Navbar from "../src/components/Navbar.jsx";
 export default function Reports() {
+  async function handleClick() {
+    const res = await axios.post('/api/auth/hook', {
+      email: "blob@gmail.com"
+    })
+    console.log(res)
+  }
   return (
     <div className="flex" id="site-content">
       <Sidebar />
       <div className="bg-gray-100 w-full">
         <Navbar />
-        Reports
+
+        <button onClick={handleClick}> Click</button>
       </div>
     </div>
   );
